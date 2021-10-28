481f4b3486ff463edc264c2d679d7414dfad7d81
Lucas Pape <lucas.pape@web.de>
2021-10-28 20:15:12 +0200

    Some shit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const config = require('./config.json');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const axios = require('axios');
+const xmlParser = require('fast-xml-parser');
+const database = require('./database.js');
+
+const PORT = 80;
+
+database.connect((err) => {
+  if(err){
+    console.log(err);
+  }else{
+    const app = express();
+
+    app.use(cors());
+    app.use(cookieParser());
+
+    app.get('/', (req, res) => {
+      database.login(json, (err, result) => {
+        if(err){
+          console.log(err);
+          res.status(500).send("Internal server error");
+        }else{
+          res.cookie('session', result.sessionId, { maxAge: 900000, httpOnly: true, sameSite: 'strict' }).redirect(req.query.origin  + '&sessionId=' + result.sessionId);
+        }
+      });
+    });
+
+    app.get('/user/:sessionId', (req, res) => {
+      const sessionId = req.params.sessionId;
+
+      if(sessionId){
+        database.getUserDetails(sessionId, (err, result) => {
+          if(err){
+            if(err === 401){
+              res.status(401).send("Unauthorized");
+            }else{
+              console.log(err);
+              res.status(500).send("Internal server error");
+            }
+          }else{
+            res.send(result);
+          }
+        });
+      }else{
+        res.status(400).send("Missing arguments");
+      }
+    });
+
+    app.listen(PORT, () => {
+      console.log('Server started on port ' + PORT);
+    });
+  }
+})
