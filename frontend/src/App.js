3a10a09e76b1affb0ab5fb2f412d386bd73fc790
Lucas Pape <lucas.pape@web.de>
2021-10-29 16:38:36 +0200

    Rout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 import Login from './Login.js';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Switch>
+        <Route exact path="/" render={(props) => <Login {...props} /> } />
+      </Switch>
     </div>
   );
 }
