b601a8de37597466c1b2396a8598e0b7a550b520
Lucas Pape <lucas.pape@web.de>
2021-10-29 16:22:01 +0200

    Add frontend

---
+++ frontend/src/Login.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const axios = require('axios');
+
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  constructor(){
+    super();
+
+    this.handleUsernameInputChange = this.handleUsernameInputChange.bind(this);
+    this.handlePasswordInputChange = this.handlePasswordInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameInputChange(event){
+    this.setState({ username: event.target.value });
+  }
+
+  handlePasswordInputChange(event){
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    
+    axios.post('https://login2.azubi.server.lan/login', { username: this.state.username, password: this.state.password }, { validateStatus: false }).then(response => {
+      console.log(response.data);
+    });
+  }
+
+  render(){
+    return(
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={this.state.username} onChange={this.handleUsernameInputChange}/>
+        </label>
+
+        <label>
+          Password:
+          <input type="password" value={this.state.password} onChange={this.handlePasswordInputChange}/>
+        </label>
+
+        <input type="submit" value="Login"/>
+      </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
